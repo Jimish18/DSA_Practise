aeee71e32c96fd3c84a82cd1aa79e3c08a517ef8
jimish1806 <jimish957@gmail.com>
2022-04-15 19:37:11 +0530

    created stack snippet

---
+++ stack/stck(snippet).cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define n 100
+
+class Stack
+{
+    int* array;
+    int top;
+
+    public:
+
+    Stack()
+    {
+        array = new int[n];
+        top = -1;
+    }
+
+    void push(int x)
+    {
+        if(top == n-1)
+        {
+            cout<<"stack OverFlow"<<endl;
+            return;
+        }
+        else
+        {
+            top++;
+            array[top] = x;
+        }
+    }
+
+    void pop()
+    {
+        if(top == -1)
+        {
+            cout<<"Stack is empty"<<endl;
+            return;
+        }
+        else
+        {
+            top--;
+        }
+    }
+
+    int Top()
+    {
+        if(top == -1)
+        {
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        return array[top];
+    }
+
+    bool empty()
+    {
+        return top==-1;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
+
