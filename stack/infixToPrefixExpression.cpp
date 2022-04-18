e6a0fa29f250466e382c1dbf4bbbd906b9569e86
jimish1806 <jimish957@gmail.com>
2022-04-18 19:42:13 +0530

    problem about infixToPrefix Conversion

---
+++ stack/infixToPrefixExpression.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prec(char c)
+{
+    if(c == '^')
+    {
+        return 3;
+    }
+    else if(c == '/' || c == '*')
+    {
+        return 2;
+    }
+    else if(c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+string infixToPrefixConversion(string s)
+{
+    reverse(s.begin(),s.end());
+    string res = "";
+    stack<char> st;
+    
+    for(int i = 0; i < s.length(); i++)
+    {
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            res += s[i];
+        }
+        else if(s[i] == ')')
+        {
+            st.push(s[i]);
+        }
+        else if(s[i] == '(')
+        {
+            while(!st.empty() && st.top() != ')')
+            {
+                res += st.top();
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while(!st.empty() && prec(st.top()) > prec(s[i]))
+            {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }       
+
+    }
+
+    while(!st.empty())
+    {
+        res += st.top();
+        st.pop();
+    }
+
+    reverse(res.begin(),res.end());
+
+    return res;
+}
+
+int main()
+{
+    string s = "(a-b/c)*(a/k-l)";
+    cout<<infixToPrefixConversion(s)<<endl;
+
+    return 0;
+}
