b32f2e37dc211db8e3ee83f4cbf27419c6e3a19f
jimish1806 <jimish957@gmail.com>
2022-04-20 00:23:54 +0530

    solved balancedParenthesis Problem

--- stack/balanceParenthesis.cpp
+++ stack/balanceParenthesis.cpp
@@ -1,66 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool matchParanthesis(char c1,char c2)
-{
-    if(c1 == '(' && c2 == ')')
-    {
-        return true;
-    }
-    if(c1 == '{' && c2 == '}')
-    {
-        return true;
-    }
-    if(c1 == '[' && c2 == ']')
-    {
-        return true;
-    }
+// bool matchParanthesis(char c1,char c2)
+// {
+//     if(c1 == '(' && c2 == ')')
+//     {
+//         return true;
+//     }
+//     if(c1 == '{' && c2 == '}')
+//     {
+//         return true;
+//     }
+//     if(c1 == '[' && c2 == ']')
+//     {
+//         return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
 bool isBalanced(string s)
 {
     stack<char> st;
+    bool res = true;
 
     for(int i = 0 ; i < s.length(); i++)
     {
-        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-        {
-            continue;
-        }
-        else if(s[i] >= '0' && s[i] <= '9')
-        {
-            continue;
-        }
-        else if(s[i] == ')' || s[i] == '}' || s[i] == ']')
-        {
-            if(st.empty())
-            {
-                return false;
-            }
-            if(matchParanthesis(st.top(),s[i]))
-            {
-                st.pop();
-            }
-        }
-        else if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
         {
             st.push(s[i]);
         }
-        else
+        else if(s[i] == ')')
         {
-            continue;
+            if(!st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                res = false;
+                break;
+            }
         }
-    }
+        else if(s[i] == '}')
+        {
+            if(!st.empty() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                res = false;
+                break;
+            }
+        }
+        else if(s[i] == ']')
+        {
+            if(!st.empty() && st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                res = false;
+                break;
+            }
+        }
+    }       
 
-    if(st.empty())
+    if(!st.empty())
     {
-        return true;
+        return false;
     }
     else
     {
-        return false;
+        return res;
     }
 
 
@@ -68,7 +82,7 @@ bool isBalanced(string s)
 
 int main()
 {
-    string s = "[1+{(2+3)-(12-25)}]";
+    string s = "[1+{2+(20+21)}]";
 
     if(isBalanced(s))
     {
