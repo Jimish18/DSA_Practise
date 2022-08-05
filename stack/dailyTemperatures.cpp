e19b42c85a12e458466da0712c050aa912b1d55b
jimish1806 <jimish957@gmail.com>
2022-08-05 09:42:07 +0530

    solved "Daily Temperatures" Problem Using Monotonic stack

---
+++ stack/dailyTemperatures.cpp
@@ -0,0 +1,63 @@
+/*
+Question - Daily Temperature
+
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number
+of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible,
+keep answer[i] == 0 instead.
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> dailyTemperatures(vector<int>& temperatures) 
+{
+    stack<int> st;
+    vector<int> ans(temperatures.size(),0);
+
+    for(int i = 0 ; i < temperatures.size() ; i++)
+    {
+        while(!st.empty())
+        {
+            if(temperatures[st.top()] < temperatures[i])
+            {
+                ans[st.top()] = i - st.top();
+                st.pop(); 
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        st.push(i);
+    }        
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> v = {73,74,75,71,69,72,76,73};
+
+    for(int i = 0 ; i < dailyTemperatures(v).size() ; i++)
+    {
+        cout<<dailyTemperatures(v)[i]<<" ";
+    }
+
+    cout<<endl;
+    return 0;
+}
