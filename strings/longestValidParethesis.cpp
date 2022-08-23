a0b29484f51c17988a3ee719b1aaf93c4aa21882
jimish1806 <jimish957@gmail.com>
2022-08-23 23:56:07 +0530

    solving longest valid paranthesis & multiply strings" Problems

--- strings/longestValidParethesis.cpp
+++ strings/longestValidParethesis.cpp
@@ -30,7 +30,57 @@ s[i] is '(', or ')'.
 #include <bits/stdc++.h>
 using namespace std;
 
+int longestValidParentheses(string s) 
+{
+    if(s.length() == 0 || s.length() == 1)
+    {
+        return 0;
+    }
+
+    stack<char> st;
+
+    int maxLength = 0;
+    int tempLength = 0;
+    st.push(s[0]);
+    tempLength++;
+
+    for(int i = 1 ; i < s.length(); i++)
+    {
+        if(tempLength >= 2 && s[i] == '(' && s[i+1] == ')' && i < s.length()-1)
+        {
+            st.push(s[i]);
+            tempLength++;
+            continue;
+        }
+
+        if(st.top() == '(' && s[i] == ')')
+        {
+            tempLength++;
+            st.push(s[i]);
+        }
+        else
+        {
+            tempLength=0;
+            tempLength++;
+            st.push(s[i]);
+        }
+
+        if(tempLength >= 2)
+        {
+            maxLength = max(tempLength,maxLength);
+        }
+    }    
+
+    
+
+    return maxLength;
+
+
+}
+
 int main()
 {
-    
+    string s = "()(()";
+    cout<<longestValidParentheses(s)<<endl;
+    return 0;
 }
