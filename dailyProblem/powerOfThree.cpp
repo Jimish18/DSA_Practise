b7e1a897012a1feaa6e49853b9f8e48d68a7e621
jimish1806 <jimish957@gmail.com>
2022-08-24 19:40:06 +0530

    solved "Power Of Three" Leetcode daily Problem

---
+++ dailyProblem/powerOfThree.cpp
@@ -0,0 +1,51 @@
+/*
+Question - Power of Three
+
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3^x.
+
+Example 1:
+Input: n = 27
+Output: true
+
+Example 2:
+Input: n = 0
+Output: false
+
+Example 3:
+Input: n = 9
+Output: true
+ 
+
+Constraints:
+-231 <= n <= 231 - 1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPowerOfThree(int n) 
+{
+    if(n <= 0)
+    {
+        return false;
+    }
+    else
+    {
+        if(n == pow(3,ceil(log(n)/log(3))))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+int main()
+{
+    cout<<isPowerOfThree(27)<<endl;
+    return 0;
+}
