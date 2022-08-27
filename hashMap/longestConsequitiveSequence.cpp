b4569c048434f1fab66df484d592137e45ef07cd
jimish1806 <jimish957@gmail.com>
2022-08-27 23:57:10 +0530

    solving "longest Consequitive Sequence" Problem

---
+++ hashMap/longestConsequitiveSequence.cpp
@@ -0,0 +1,52 @@
+/*
+Question - Longest Consequitive Sequence
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void longestConsecutive(vector<int>& nums) 
+{
+    map<int,int> mp;
+
+    for(int i = 0 ; i < nums.size(); i++)
+    {
+        mp[nums[i]]++;
+    }
+
+    for(auto i : mp)
+    {
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+
+}
+
+int main()
+{
+    vector<int> v = {100,4,200,1,3,2};
+
+    longestConsecutive(v);
+    return 0;
+}
