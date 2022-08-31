69c49656bb530cf6d337a744baf764ef1a021eda
jimish1806 <jimish957@gmail.com>
2022-09-01 00:53:32 +0530

    solving "intersection of two array" Problem using unordered map

--- binarySearch/intersectionOfTwoArrays.cpp
+++ binarySearch/intersectionOfTwoArrays.cpp
@@ -23,24 +23,112 @@ Constraints:
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+void intersection(vector<int>& nums1, vector<int>& nums2) 
 {
     vector<int > res;
+    unordered_map<int,int> umap;
+    
+
+    sort(nums1.begin(),nums1.end());
+
+    for(int i = 0 ; i < nums1.size(); i++)
+    {
+        cout<<nums1[i]<<" ";
+    }
+    cout<<endl;
+    sort(nums2.begin(),nums2.end());
 
     if(nums1.size() > nums2.size())
     {
-        int s = 0;
-        int e = nums1.size()-1;
-        int m = s + (e-s)/2;
         int i = 0;
+        
         while(i < nums2.size())
         {
+            int s = 0;
+            int e = nums1.size();
+            int n = nums2.size();
+            int m = s + (e-s)/2;
 
+            while(s <= e)
+            {
+                if(nums1[m] == nums2[i])
+                {
+                    if(umap[nums1[m]] < 1)
+                    {
+                        umap[nums1[m]]++;
+                    }
+                }
+                
+                if(nums1[m] < nums2[i])
+                {
+                    s = (m+1)%n;
+                }
+                else
+                {
+                    e = (m+n-1)%n;
+                }
+
+                m = s + (e-s)/2;
+            }
         }
-    }      
+
+        i++;
+    }    
+    else
+    {
+        int i = 0;
+        
+        while(i < nums1.size())
+        {
+            int s = 0;
+            int e = nums2.size();
+            int n = nums1.size();
+            int m = s + (e-s)/2;
+
+            while(s <= e)
+            {
+                if(nums2[m] == nums1[i])
+                {
+                    if(umap[nums2[m]] < 1)
+                    {
+                        umap[nums2[m]]++;
+                    }
+                }
+                
+                if(nums2[m] < nums1[i])
+                {
+                    s = (m+1)%n;
+                }
+                else
+                {
+                    e = (m+n-1)%n;
+                }
+
+                m = s + (e-s)/2;
+            }
+        }
+
+        i++;
+    } 
+
+    // for(auto i : umap)
+    // {
+    //     cout<<i.first<<" ";
+    // }
+    // cout<<endl;
+    
+
+    
+
+    // return res; 
 }
 
 int main()
 {
+    vector<int > nums1 = {4,9,5};
+    vector<int > nums2 = {9,4,9,8,4};
+
+    intersection(nums1,nums2);
+
     return 0;
 }
