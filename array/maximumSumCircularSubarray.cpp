eb0e85f9f6c080e452d8f120e6f53912dd2477b0
jimish1806 <jimish957@gmail.com>
2022-09-10 22:04:33 +0530

    solved "maximum Sum Circular Subarray" Problem

--- array/maximumSumCircularSubarray.cpp
+++ array/maximumSumCircularSubarray.cpp
@@ -8,21 +8,22 @@ Link - https://leetcode.com/problems/maximum-sum-circular-subarray/
 using namespace std;
 
 int kadane(vector<int> v)
-{
-    int sum = 0;
-    int maxSum = 0;
+{   
+    int sum = v[0];
+    int maxSum = v[0];
 
-    for(int i = 0 ; i < v.size(); i++)
+    for(int i = 1 ; i < v.size(); i++)
     {
-        sum += v[i];
-        maxSum = max(sum,maxSum);
         if(sum < 0)
         {
             sum = 0;
         }
+        sum += v[i];
+        maxSum = max(sum,maxSum);
     }
 
     return maxSum;
+
 }
 
 int maxSubarraySumCircular(vector<int>& nums) 
@@ -30,7 +31,7 @@ int maxSubarraySumCircular(vector<int>& nums)
     int nonWrapSum = kadane(nums);
     int wrapSum;
     int totalSum = 0;
-    
+
     for(int i = 0; i < nums.size(); i++)
     {
         totalSum += nums[i];
@@ -39,12 +40,19 @@ int maxSubarraySumCircular(vector<int>& nums)
 
     wrapSum = totalSum + kadane(nums);
 
+    if(wrapSum == 0)
+    {
+        return nonWrapSum;
+    }
+
     return max(nonWrapSum,wrapSum);
 }
+    
+
 
 int main()
 {
     vector<int> v = {-3,-2,-3};
-    cout<<kadane(v)<<endl;
+    cout<<maxSubarraySumCircular(v)<<endl;
     return 0;
 }
