ac902e3a358a1f8a761c6bc17190e40a4dc1f4d2
jimish1806 <jimish957@gmail.com>
2022-09-14 22:03:07 +0530

    optimized "set Matrix Zeroes" Problem code

--- matrix/setMatrixZeroes.cpp
+++ matrix/setMatrixZeroes.cpp
@@ -8,39 +8,72 @@ using namespace std;
 
 void setZeroes(vector<vector<int>>& matrix) 
 {
-    set<int> zCol;
-    set<int> zRow;
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-    for(int i = 0 ; i < n; i++)
+    bool row = false, col = false;
+    for(int i = 0; i < matrix.size(); i++)
     {
-        for(int j = 0; j < m ; j++)
+        for(int j = 0; j < matrix[0].size(); j++)
         {
-            if(matrix[i][j] == 0)
+            if(matrix[i][j] == 0) 
             {
-                zCol.insert(j);
-                zRow.insert(i);
+                if(i == 0) row = true;
+                if(j == 0) col = true;
+                matrix[0][j] = matrix[i][0] = 0;
             }
         }
     }
-
-    for(auto x : zCol)
+    for(int i = 1; i < matrix.size(); i++)
     {
-        for(int i = 0; i < n ; i++)
+        for(int j = 1; j < matrix[0].size(); j++)
         {
-            matrix[i][x] = 0;
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
         }
     }
-
-    for(auto x : zRow)
+    if(col)
     {
-        for(int i = 0; i < m ; i++)
-        {
-            matrix[x][i] = 0;
-        }
+        for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
+    }
+    if(row)
+    {
+        for(int j = 0; j < matrix[0].size(); j++) matrix[0][j] = 0;
     }
 }
+
+// Space Complexity = O(m+n)
+// void setZeroes(vector<vector<int>>& matrix) 
+// {
+//     set<int> zCol;
+//     set<int> zRow;
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+
+//     for(int i = 0 ; i < n; i++)
+//     {
+//         for(int j = 0; j < m ; j++)
+//         {
+//             if(matrix[i][j] == 0)
+//             {
+//                 zCol.insert(j);
+//                 zRow.insert(i);
+//             }
+//         }
+//     }
+
+//     for(auto x : zCol)
+//     {
+//         for(int i = 0; i < n ; i++)
+//         {
+//             matrix[i][x] = 0;
+//         }
+//     }
+
+//     for(auto x : zRow)
+//     {
+//         for(int i = 0; i < m ; i++)
+//         {
+//             matrix[x][i] = 0;
+//         }
+//     }
+// }
 
 int main()
 {
