51350d9598c99d5c02bf4e9baffa1865a4bb4f2e
jimish1806 <jimish957@gmail.com>
2022-10-01 17:04:34 +0530

    solving "path sum II" problem

---
+++ binaryTree/pathSum2.cpp
@@ -0,0 +1,78 @@
+/*
+Question - path Sum II
+
+Link - https://leetcode.com/problems/path-sum-ii/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode 
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {};
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {};
+};
+
+void calcPathSum(TreeNode* root,vector<vector<int>> &result,vector<int> &path,int targetSum,int curSum)
+{
+    if(!root)
+    {
+        return;
+    }
+
+    curSum += root->val;
+
+    path.push_back(root->val);
+
+    if(!root->left && !root->right && curSum == targetSum)
+    {
+        result.push_back(path);
+        path.clear();
+        return;
+    }
+
+    calcPathSum(root->left,result,path,targetSum,curSum);    
+    path.pop_back();
+    calcPathSum(root->right,result,path,targetSum,curSum);    
+    
+}
+
+vector<vector<int>> pathSum(TreeNode* root, int targetSum) 
+{
+    vector<vector<int>> result;   
+    vector<int> path;
+
+    calcPathSum(root,result,path,targetSum,0);
+
+    return result;     
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->right->right->left = new TreeNode(5);
+    root->right->right->right = new TreeNode(1);
+
+    vector<vector<int>> v = pathSum(root,22);
+
+    for(int i = 0 ;i < v.size(); i++)
+    {
+        for(int j = 0 ; j < v[i].size(); j++)
+        {
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
