9e87e62ccde76c9ed1392d7454c22146782488a4
jimish1806 <jimish957@gmail.com>
2022-10-11 02:14:47 +0530

    solved "count Complete Treenode" Problem

---
+++ binaryTree/countCompleteTreeNodes.cpp
@@ -0,0 +1,53 @@
+/*
+Question - Count Complete Tree Node
+
+Link - https://leetcode.com/problems/count-complete-tree-nodes/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode 
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {};
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {};
+};
+
+
+
+int countNodes(TreeNode* root) 
+{
+    if(!root)
+    {
+        return 0;
+    }
+
+    int hl = 0,hr = 0;
+
+    TreeNode* l = root, *r = root;
+
+    while(l)
+    {
+        hl++;
+        l = l->left;
+    }
+
+    while(r)
+    {
+        hr++;
+        r = r->right;
+    }
+
+    if(hl == hr) return pow(2,hl)-1;
+
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+int main()
+{
+    return 0;
+}
