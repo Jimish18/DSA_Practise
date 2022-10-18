29956be181f247b05d8d5038ff151d0d5ddfe72d
jimish1806 <jimish957@gmail.com>
2022-10-18 22:32:45 +0530

    optimized "search 2D Matrix" Problem Time Complexity from O(m+n) to O(log(m*n))

--- matrix/search2DMatrix.cpp
+++ matrix/search2DMatrix.cpp
@@ -7,19 +7,49 @@ Link - https://leetcode.com/problems/search-a-2d-matrix/
 #include <bits/stdc++.h>
 using namespace std;
 
+// better Approach
+// bool searchMatrix(vector<vector<int>>& matrix, int target) 
+// {
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+//     int i = 0,j = m-1;
+
+//     while(i < n && j >= 0)
+//     {
+//         if(matrix[i][j] == target) return true;
+
+//         if(target < matrix[i][j]) j--;
+//         else if(target > matrix[i][j]) i++;
+        
+//     }
+//     return false;        
+// }
+
+// log(m*n) Approach
 bool searchMatrix(vector<vector<int>>& matrix, int target) 
 {
     int n = matrix.size();
     int m = matrix[0].size();
-    int i = 0,j = m-1;
+    int start = 0;
+    int end = (n*m)-1;
+    int mid;
 
-    while(i < n && j >= 0)
+    while(start <= end)
     {
-        if(matrix[i][j] == target) return true;
+        mid = start - (start-end)/2;
 
-        if(target < matrix[i][j]) j--;
-        else if(target > matrix[i][j]) i++;
-        
+        if(matrix[mid/m][mid%m] == target)
+        {
+            return true;
+        }
+        else if(matrix[mid/m][mid%m] > target)
+        {
+            end = mid-1;
+        }
+        else
+        {
+            start = mid+1;
+        }
     }
     return false;        
 }
@@ -27,6 +57,6 @@ bool searchMatrix(vector<vector<int>>& matrix, int target)
 int main()
 {
     vector<vector<int>> v = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-    cout<<searchMatrix(v,13)<<endl;
+    cout<<searchMatrix(v,7)<<endl;
     return 0;
 }
