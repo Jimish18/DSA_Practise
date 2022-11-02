f91acc45e10af5f7d2467ae1e9b2e3fc693ef198
jimish1806 <jimish957@gmail.com>
2022-11-02 23:59:11 +0530

    solved "merge Sorted Array" Problem

--- array/mergeSortedArray.cpp
+++ array/mergeSortedArray.cpp
@@ -13,9 +13,9 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
     int j = n-1;
     int k = m+n-1;
 
-    while(i >= 0 && j >= 0)
+    while(j >= 0)
     {
-        if(nums1[i] >= nums2[j])
+        if(i >= 0 && nums1[i] >= nums2[j])
         {
             nums1[k] = nums1[i];
             k--;
@@ -23,7 +23,7 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
         }
         else
         {
-            nums[k] = nums2[j];
+            nums1[k] = nums2[j];
             k--;
             j--;
         }
