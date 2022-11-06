0268f4b74a027d9ef556b11ddf7f1f787cb39743
jimish1806 <jimish957@gmail.com>
2022-11-07 00:53:24 +0530

    modified "find the duplicate Number" problem with Linked List Cycle Approach

--- LnkdLst/findTheDuplicateNumber.cpp
+++ LnkdLst/findTheDuplicateNumber.cpp
@@ -8,18 +8,41 @@ Link - https://leetcode.com/problems/find-the-duplicate-number/
 using namespace std;
 
 // brute Force Approach
+// int findDuplicate(vector<int>& nums) 
+// {
+//    sort(nums.begin(),nums.end());
+//    for(int i = 1 ; i < n; i++)
+//    {
+//         if(nums[i] == nums[i-1])
+//         {
+//             return nums[i];
+//         }
+//    }     
+
+//    return -1;
+// }
+
+// Linked List Cycle Method
 int findDuplicate(vector<int>& nums) 
 {
-   sort(nums.begin(),nums.end());
-   for(int i = 1 ; i < n; i++)
-   {
-        if(nums[i] == nums[i-1])
-        {
-            return nums[i];
-        }
-   }     
+    int fast = nums[0];
+    int slow = nums[0];
+   
+    do
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }while(slow != fast);
 
-   return -1;
+    fast = nums[0];
+
+    while(fast != slow)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
 }
 
 int main()
