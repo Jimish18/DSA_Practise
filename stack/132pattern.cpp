e2f76cfa65c5e3d36d3aac3531dcc689f6638b6b
jimish1806 <jimish957@gmail.com>
2022-11-11 00:27:27 +0530

    solved "132 Pattern" Problem Using stack

--- stack/132pattern.cpp
+++ stack/132pattern.cpp
@@ -1,3 +1,39 @@
 /*
-Question - 
+Question - 132 Pattern
+
+Link - https://leetcode.com/problems/132-pattern/
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool find132pattern(vector<int>& nums) 
+{
+    int s3 = INT_MIN;
+    stack<int> st;   
+    int n = nums.size();
+
+    for(int i = n-1; i >= 0; i--)
+    {
+        if(nums[i] < s3)
+        {
+            return true;
+        }
+        else
+        {
+            while(!st.empty() && nums[i] > st.top())
+            {
+                s3 = st.top();
+                st.pop();
+            }
+        }
+        st.push(nums[i]);
+    }  
+
+    return false;   
+}
+
+int main()
+{
+    return 0;
+}
