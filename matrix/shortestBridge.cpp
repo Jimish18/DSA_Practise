e0f7e5d87e361f9896d6526a546724f2060e15cc
jimish1806 <jimish957@gmail.com>
2022-11-23 18:02:51 +0530

    solved "shortest Bridge" Problem using DFS & BFS

--- matrix/shortestBridge.cpp
+++ matrix/shortestBridge.cpp
@@ -8,23 +8,22 @@ Link - https://leetcode.com/problems/shortest-bridge/
 using namespace std;
 
 int n,m;
-vector<int> dc = {0,0,1,-1};
 vector<int> dr = {1,-1,0,0};
+vector<int> dc = {0,0,1,-1};
 
-void dfs(int i , int j , queue<pair<int,int>> &q,vector<vector<int>>& grid)
+void dfs(int i , int j , queue<pair<int,int>> &q,vector<vector<int>>& grid,vector<vector<bool>> &visit)
 {
-    if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0 || grid[i][j] == -1) return;
-
-    grid[i][j] = -1;
-
+    visit[i][j] = true;
     q.push({i,j});
 
-    for(int k = 0 ; k < 4 ; k++)
+    for(int k = 0 ; k < 4; k++)
     {
         int newRow = i + dr[k];
         int newCol = j + dc[k];
 
-        dfs(newRow,newCol,q,grid);
+        if(newCol < 0 || newRow < 0 || newRow >= n || newCol >= m || visit[newRow][newCol] == true || grid[newRow][newCol] == 0) continue;
+
+        dfs(newRow,newCol,q,grid,visit);
     }
     
 }
@@ -33,6 +32,8 @@ int shortestBridge(vector<vector<int>>& grid)
 {
     n = grid.size();
     m = grid[0].size();
+    vector<bool> temp (m,false);
+    vector<vector<bool>> visit(n,temp);
     bool flag  = false;
     int level = 0;
 
@@ -41,19 +42,22 @@ int shortestBridge(vector<vector<int>>& grid)
 
     for(int i = 0; i < n && !flag; i++)
     {
-        for(int j = 0 ; j < m && !flag; j++)
+        for(int j = 0; j < m && !flag ; j++)
         {
-            if(grid[i][j]) dfs(i,j,q,grid);
-            flag = true;
+            if(grid[i][j] == 1)
+            {
+                dfs(i,j,q,grid,visit);
+                flag = true;
+            }
         }
-    }
+    }       
 
     while(!q.empty())
     {
-        level++;
         N = q.size();
+        level++;
 
-        for(int i = 0; i < N ; i++)
+        for(int i = 0 ; i < N; i++)
         {
             auto temp = q.front();
             q.pop();
@@ -61,27 +65,38 @@ int shortestBridge(vector<vector<int>>& grid)
             int row = temp.first;
             int col = temp.second;
 
-            grid[row][col] = -1;
-
-            for(int j = 0; j < 4; j++)
+            for(int j = 0 ; j < 4; j++)
             {
                 int newRow = row + dr[j];
                 int newCol = col + dc[j];
 
-                if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < m)
-                {
-                    if(grid[newRow][newCol] == 1) return level;
+                if(newRow < 0 || newCol < 0 || newRow >= n || newCol >= m || visit[newRow][newCol] == true) continue;
 
-                    if(grid[newRow][newCol] == 0) q.push({newRow,newCol});
-                }
+                if(grid[newRow][newCol] == 1) return (level-1);
+
+                q.push({newRow,newCol});
+                visit[newRow][newCol] = true;
             }
         }
-    }
+    }    
 
-    return -1;            
+    return -1;   
 }
 
 int main()
 {
+    vector<vector<int>> v = {{1,1,1,1,1},{1,0,0,0,1},{1,0,1,0,1},{1,0,0,0,1},{1,1,1,1,1}};
+    // {1,1,1,1,1},{1,0,0,0,1},{1,0,1,0,1},{1,0,0,0,1},{1,1,1,1,1}
+    // {0,1,0},{0,0,0},{0,0,1}
+    cout<<shortestBridge(v)<<endl;
+
+    for(int i = 0; i < v.size(); i++)
+    {
+        for(int j = 0 ; j < v[i].size(); j++)
+        {
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
