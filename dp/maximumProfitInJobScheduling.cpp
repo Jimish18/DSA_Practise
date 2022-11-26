f23987b801292d08f268d7ff4b67866cfa9c2164
jimish1806 <jimish957@gmail.com>
2022-11-27 02:08:50 +0530

    optimized "maximum Profit in job scheduling" problem code from O(n^2) to O(n*log(n))

--- dp/maximumProfitInJobScheduling.cpp
+++ dp/maximumProfitInJobScheduling.cpp
@@ -8,6 +8,74 @@ Link - https://leetcode.com/problems/maximum-profit-in-job-scheduling/
 using namespace std;
 
 // o(n^2) Approach
+// int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) 
+// {
+//     int n = startTime.size();
+//     vector<vector<int>> jobs;
+    
+//     for(int i = 0; i < n; i++)
+//     {
+//         jobs.push_back({endTime[i],startTime[i],profit[i]});
+//     }
+
+//     sort(jobs.begin(),jobs.end());
+
+//     int result = 0;
+//     vector<int> dp(n);
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         dp[i] = jobs[i][2];
+//         if(i == 0)
+//         {
+//             result = max(result,dp[i]);
+//             continue;
+//         }
+
+//         dp[i] = max(dp[i],dp[i-1]);
+
+//         int currTime = jobs[i][1];
+
+//         for(int j = i-1; j >= 0; j--)
+//         {
+//             if(jobs[j][0] <= currTime)
+//             {
+//                 dp[i] = max(dp[i],(dp[j]+jobs[i][2]));
+//                 break;
+//             }
+//         }
+
+//         result = max(result,dp[i]);
+//     }       
+
+//     return result;
+// }
+
+// O((n*Log(n)))
+int search(vector<vector<int>> &jobs,int curr)
+{
+    int l = 0 ; 
+    int r = curr-1;
+    int ans = -1;
+
+    while(l <= r)
+    {
+        int mid = (l+r)/2;
+
+        if(jobs[mid][0] <= jobs[curr][1])
+        {
+            ans = mid;
+            l = mid+1;
+        }
+        else
+        {
+            r = mid-1;
+        }
+    }
+
+    return ans;
+}
+
 int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) 
 {
     int n = startTime.size();
@@ -20,31 +88,18 @@ int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& pro
 
     sort(jobs.begin(),jobs.end());
 
-    int result = 0;
-    vector<int> dp(n);
+    vector<int> dp(n,0);
+    dp[0] = jobs[0][2];
+    int result = dp[0];
 
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i < n; i++)
     {
-        dp[i] = jobs[i][2];
-        if(i == 0)
-        {
-            result = max(result,dp[i]);
-            continue;
-        }
-
+        int incl = jobs[i][2];
+        dp[i] = incl;
+        int curr = i;
+        int l = search(jobs,curr);
+        if(l != -1) dp[i] += dp[l];
         dp[i] = max(dp[i],dp[i-1]);
-
-        int currTime = jobs[i][1];
-
-        for(int j = i-1; j >= 0; j--)
-        {
-            if(jobs[j][0] <= currTime)
-            {
-                dp[i] = max(dp[i],(dp[j]+jobs[i][2]));
-                break;
-            }
-        }
-
         result = max(result,dp[i]);
     }       
 
