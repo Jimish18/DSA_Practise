c83d37ddac2912d4751c13d8dcf4ae126a4e29cd
jimish1806 <jimish957@gmail.com>
2022-11-29 02:00:20 +0530

    solved "partition Equal Subset Sum" Problem Using DP Tabulation

---
+++ dp/partitionEqualSubsetSum.cpp
@@ -0,0 +1,62 @@
+/*
+Question - Partition Equal Subset Sum
+
+Link - https://leetcode.com/problems/partition-equal-subset-sum/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canPartition(vector<int>& nums) 
+{
+    int sum = 0; 
+    int n = nums.size();
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        sum += nums[i];
+    }        
+
+    if(sum%2 != 0) return false;
+    else
+    {   
+        sum /= 2;
+        vector<bool> temp(sum+1);
+        vector<vector<bool>> dp(n+1,temp);
+
+        for(int i = 0; i < n+1; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        for(int i = 0; i < sum+1; i++)
+        {
+            dp[0][i] = false;
+        }
+
+        for(int i = 1; i < n+1; i++)
+        {
+            for(int j = 1; j < sum+1; j++)
+            {
+                if(nums[i-1] <= j)
+                {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else if(nums[i-1] > j)
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+            
+        return dp[n][sum];  
+    }
+
+    return false;
+
+}
+
+int main()
+{
+    return 0;
+}
