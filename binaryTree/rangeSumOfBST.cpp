457dc3759b2de6ed5980ed17719f4ae004ab2364
jimish1806 <jimish957@gmail.com>
2022-12-07 13:26:28 +0530

    solved "range Sum Of BST" leetcode daily Problem using DFS

---
+++ binaryTree/rangeSumOfBST.cpp
@@ -0,0 +1,41 @@
+/*
+Question - Range Sum Of BST
+
+Link - https://leetcode.com/problems/range-sum-of-bst/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode 
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {};
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {};
+};
+
+int sum = 0;
+
+void inorder(TreeNode* root, int low, int high)
+{
+    if(!root) return;
+
+    inorder(root->left,low,high);    
+    if(root->val >= low && root->val <= high) sum += root->val;
+    inorder(root->right,low,high);
+}
+
+int rangeSumBST(TreeNode* root, int low, int high) 
+{
+    inorder(root,low,high);
+    return sum;
+}
+
+int main()
+{
+    return 0;
+}
