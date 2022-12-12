12f76c92431dadf972765abeedc9718a2f56957c
jimish1806 <jimish957@gmail.com>
2022-12-12 23:31:28 +0530

    solving "climbing Stairs" problem using Memoization

--- dp/climbingStairs.cpp
+++ dp/climbingStairs.cpp
@@ -8,34 +8,42 @@ Link - https://leetcode.com/problems/climbing-stairs/
 #include <bits/stdc++.h>
 using namespace std;
 
-// int cnt = 0;
+void countAttemt(int n,vector<int> &dp)
+{
+    if(n <= 0) return;
 
-// void countAttemt(int n)
-// {
-//     if(n < 0) return;
-//     if(n == 0)
-//     {
-//         cnt++;
-//         return;
-//     }
+    if(dp[n-1] != -1)
+    {
+        dp[n] = dp[n-1];
+        return;
+    } 
 
-//     countAttemt(n-1);
-
-//     countAttemt(n-2);
-// }
+    if(n >= 2)
+    {
+        dp[n] = dp[n-1] + dp[n-2];
+    }
+    else if(n >= 1)
+    {
+        dp[n] = dp[n-1] + 1;
+    }
+}
 
 int climbStairs(int n) 
 {
-    // countAttemt(n);
-    vector<int> dp(n+1);
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
+    // Tabulation
+    // vector<int> dp(n+2);
+    // dp[0] = 0;
+    // dp[1] = 1;
+    // dp[2] = 2;
 
-    for(int i = 3; i < n+1; i++)
-    {
-        dp[i] = dp[i-1]+dp[i-2];
-    }
+    // for(int i = 3; i < n+1; i++)
+    // {
+    //     dp[i] = dp[i-1]+dp[i-2];
+    // }
+
+    // Memoization
+    vector<int> dp(n+2,-1);
+
 
     return dp[n];
 }
