10ba351265887a1010f06d743e9582524e04c444
jimish1806 <jimish957@gmail.com>
2022-12-18 13:26:44 +0530

    solved "product of Array Except Self" Problem Using Prefix Sum Concept

---
+++ array/productOfArrayExceptSelf.cpp
@@ -0,0 +1,65 @@
+/*
+Question - Product Of Array Except Self
+
+Link - https://leetcode.com/problems/product-of-array-except-self/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// O(n) time and O(n) space
+vector<int> productExceptSelf(vector<int>& nums) 
+{
+    int n = nums.size();
+    vector<int> result;
+    vector<int> preFix(n+1,1);
+    vector<int> suFix(n+1,1);
+    // int 
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        preFix[i+1] = preFix[i]*nums[i];
+        suFix[(n+1)-i-2] = suFix[(n+1)-i-1]*nums[n-i-1];
+    }
+
+
+    for(int i = 0; i < n; i++)
+    {
+        result.push_back(preFix[i]*suFix[i+1]);
+    }
+
+    return result;
+}
+
+
+vector<int> productExceptSelf(vector<int>& nums) 
+{
+    int n = nums.size();
+    vector<int> result;
+    vector<int> preFix(n+1,1);
+    vector<int> suFix(n+1,1);
+    // int 
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        preFix[i+1] = preFix[i]*nums[i];
+        suFix[(n+1)-i-2] = suFix[(n+1)-i-1]*nums[n-i-1];
+    }
+
+
+    for(int i = 0; i < n; i++)
+    {
+        result.push_back(preFix[i]*suFix[i+1]);
+    }
+
+    return result;
+}
+
+
+
+int main()
+{
+    vector<int> nums = {-1,1,0,-3,3};
+    productExceptSelf(nums);
+    return 0;
+}
