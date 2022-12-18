0a2d8bd028f8d247f5f507abfbcea6d0c272bd2e
jimish1806 <jimish957@gmail.com>
2022-12-18 15:56:12 +0530

    solving "Subarray Sum Equals K" problem

---
+++ array/subarraySumEqualsK.cpp
@@ -0,0 +1,25 @@
+/*
+Question - Subarray Sum Equals K
+
+Link - https://leetcode.com/problems/subarray-sum-equals-k/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraySum(vector<int>& nums, int k) 
+{
+    int preFix = 0;        
+    int n = nums.size();
+
+    for(int i = 0; i < n; i++)
+    {
+        
+    }
+    
+}
+
+int main()
+{
+    return 0;
+}
