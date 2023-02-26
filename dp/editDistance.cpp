d46f10209bc44e4b2a3602b48678d83cd2ec9190
jimish1806 <jimish957@gmail.com>
2023-02-27 01:38:08 +0530

    solved "edit Distance" leetcode daily problem using dp

--- dp/editDistance.cpp
+++ dp/editDistance.cpp
@@ -7,52 +7,25 @@ Link - https://leetcode.com/problems/edit-distance/
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(string word1, string word2,int i,int j,int &result)
+int dfs(string word1, string word2,int i,int j,vector<vector<int>> &dp)
 {
     if(i < 0 || j < 0)
     {
-        if(i < 0) result += j+1;
-        else if (j < 0) result += i + 1;
-    
-        return;
+        if(i < 0 && j < 0) return 0;
+        else if(i < 0) return j+1;
+        else if (j < 0) return i + 1;
     }
 
-    if(i == j)
+    if(dp[i][j] != -1) return dp[i][j];
+
+    while(word1[i] == word2[j])
     {
-        if(word1[i] == word2[j])
-        {
-            dfs(word1,word2,i-1,j-1,result);
-        }
-        else
-        {
-            result += 1;
-            result += 1 + dfs(word1,word2,i-1,j-1,result);
-        }
-    }
-    else if(j > i)
-    {
-        if(word1[i] == word2[j])
-        {
-            result += 0 + dfs(word1,word2,i-1,j-1,result);
-        }
-        else
-        {
-            result += 1 +dfs(word1,word2,i,j-1,result);
-        }
-    }
-    else if(j < i)
-    {
-        if(word1[i] == word2[j])
-        {
-            result += 0 + dfs(word1,word2,i-1,j-1,result);
-        }
-        else
-        {
-            result += 1 +dfs(word1,word2,i-1,j,result);
-        }
+        i--;j--;        
     }
 
-    return result;
+    dp[i][j] = min(1 + dfs(word1,word2,i,j-1,dp) , min(1 + dfs(word1,word2,i-1,j,dp) , 1 + dfs(word1,word2,i-1,j-1,dp)));
+
+    return dp[i][j];
 }
 
 int minDistance(string word1, string word2) 
@@ -60,8 +33,9 @@ int minDistance(string word1, string word2)
     int result = 0;
     int n = word1.length();
     int m = word2.length();
+    vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
 
-    return dfs(word1,word2,n-1,m-1,result);
+    return dfs(word1,word2,n-1,m-1,dp);
 }
 
 int main()
