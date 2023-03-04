5b6f3b8c9f95832fb2a92b458400a56a48547f7e
jimish1806 <jimish957@gmail.com>
2023-03-04 20:39:26 +0530

    solving "count Subarrays with Fixed Bounds" leetcode daily

---
+++ slidingWindow/countSubarraysWithFixedBounds.cpp
@@ -0,0 +1,26 @@
+/*
+Question - Count Subarrays With Fixed Bounds
+
+Link - https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long countSubarrays(vector<int>& nums, int minK, int maxK) 
+{
+    long long count = 0;
+    int i = 0;
+    int j = 0;
+    int n = nums.size();    
+
+    while(j < n)
+    {
+        
+    }        
+}
+
+int main()
+{
+    return 0;
+}
