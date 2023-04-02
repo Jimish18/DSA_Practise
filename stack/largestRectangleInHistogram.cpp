244d5ae47530320e3f1cff4bb4ac5dd43b68679e
jimish1806 <jimish957@gmail.com>
2023-04-02 17:53:54 +0530

    modified "largest Rectangle In Histogram" Problem Using Stack

--- stack/largestRectangleInHistogram.cpp
+++ stack/largestRectangleInHistogram.cpp
@@ -7,99 +7,64 @@ Link - https://leetcode.com/problems/largest-rectangle-in-histogram/
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> previosSmallerElement(vector<int> array)
+vector<int> nextSmallerToRight(vector<int> temp)
 {
-    vector<int> result;
+    vector<int> result(temp.size()); 
     stack<int> st;
-    array.insert(array.begin(),-1);
 
-    int i = 1;
-    st.push(0);
-    while(i < array.size())
+    for(int i = temp.size()-1 ; i >= 0; i--)
     {
-        while(!st.empty() && array[st.top()] > array[i])
+        while(!st.empty() && temp[st.top()] >= temp[i])
         {
             st.pop();
         }
-        result.push_back(array[st.top()]);
+        if(st.empty()) result[i] = temp.size();
+        else result[i] = st.top();
+        
         st.push(i);
-        i++;
     }
 
     return result;
 }
 
-vector<int> nextSmallerElement(vector<int> array)
+vector<int> nextSmallerToLeft(vector<int> temp)
 {
-    vector<int> result;
+    vector<int> result(temp.size());
     stack<int> st;
-    result.insert(result.begin(),array[array.size()-1]);
-    int i = array.size()-2;
-    st.push(array.size()-1);
 
-    while(i >= 0)
+    for(int i = 0; i < temp.size(); i++)
     {
-        while(!st.empty() && array[st.top()] < array[i])
+        while(!st.empty() && temp[st.top()] >= temp[i])
         {
-            // st.pop();
-            // result.insert(result.begin(),array[st.top()]);
+            st.pop();
         }
-        result.insert(result.begin(),array[st.top()]);
+
+        if(st.empty()) result[i] = -1;
+        else result[i] = st.top();
+
         st.push(i);
-        i--;
     }
 
     return result;
 }
-
 
 int largestRectangleArea(vector<int>& heights) 
 {
-    // Approach 1
-    // int i = 0 ;
-    // stack<int> st;
-    // int maxArea = 0;    
-    // heights.push_back(0);
-    // while(i < heights.size())
-    // {
-    //     while(!st.empty() && heights[st.top()] > heights[i])
-    //     {
-    //         int t = st.top();
-    //         int h = heights[t];
-    //         st.pop();
-    //         if(st.empty())
-    //         {
-    //             maxArea = max(maxArea,h*i);
-    //         }
-    //         else
-    //         {
-    //             int tempLength = i-st.top()-1; 
-    //             maxArea = max(maxArea,h*tempLength);
-    //         }
-    //     }
-    //     st.push(i);
-    //     i++; 
-    // } 
-
-    // Approach 2
+    vector<int> right = nextSmallerToRight(heights);
+    vector<int> left = nextSmallerToLeft(heights);
     int maxArea = 0;
-    vector<int> previousSmaller = previosSmallerElement(heights);
-    vector<int> nextSmaller = nextSmallerElement(heights);
 
-    for(int i = 0 ;i < heights.size(); i++)
+    for(int i = 0; i < heights.size(); i++)
     {
-        int curr = (nextSmaller[i] - previousSmaller[i] -1)*heights[i];
-        maxArea = max(maxArea,curr); 
+        maxArea = max(maxArea , (right[i]-left[i]-1)*heights[i]);
     }
 
-    return maxArea;       
+    return maxArea;
 }
 
 int main()
 {
-    vector<int> temp = {2,1,5,6,2,3};
-    // {2,4}
-    // {2,1,5,6,2,3}
+    vector<int> temp = {6,2,5,4,5,1,6};
     cout<<largestRectangleArea(temp)<<endl;
 
     return 0;
