6e64738d506633df7dff8f1d725aec5acac82382
jimish1806 <jimish957@gmail.com>
2023-05-19 01:43:55 +0530

    solved "longest repeating Subsequence" Problem using lcs

---
+++ dp/longestRepeatingSubsequence.cpp
@@ -0,0 +1,37 @@
+/*
+Question - Longest Repeating Subsequence
+
+Link - https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int LongestRepeatingSubsequence(string str)
+{
+    string temp = str;
+    
+    int n = str.length();
+    vector<vector<int>> dp(n+1 , vector<int> (n+1));
+    
+    for(int i = 0 ; i <= n; i++)
+    {
+        for(int j = 0 ; j <= n; j++)
+        {
+            if(i == 0 || j == 0) dp[i][j] = 0;
+            else
+            {
+                if((str[i-1] == temp[j-1]) && (i != j)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+    
+    return dp[n][n];
+}
+
+
+int main()
+{
+    return 0;
+}
