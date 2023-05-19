a8680cc02ac686ccd0a7884d221421ab6fd22874
jimish1806 <jimish957@gmail.com>
2023-05-20 01:46:07 +0530

    solved "is graph bipartite?" leetcode daily Question

---
+++ graph/isGraphBipartite.cpp
@@ -0,0 +1,22 @@
+/*
+Question - Is Graph Bipartite ?
+
+Link - https://leetcode.com/problems/is-graph-bipartite/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBipartite(vector<vector<int>>& graph) 
+{
+    int n = graph.size();
+    vector<int> visited(n,0);
+    queue<int> q;
+    q.push(0);       
+}
+
+
+int main()
+{
+    return 0;
+}
