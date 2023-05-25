0939f22fd6b719385fb20043dbd39b3a8e4dd13c
jimish1806 <jimish957@gmail.com>
2023-05-25 17:15:36 +0530

    solving "minimum Score Triangulation of Polygon" Problem

---
+++ dp/minimumScoreTriangulationOfPolygon.cpp
@@ -0,0 +1,28 @@
+/*
+Qustion - Minimum Score Triangulation of Polygon
+
+
+Link - https://leetcode.com/problems/minimum-score-triangulation-of-polygon/description/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& values,int i,int j)
+{
+    if(i >= j-1) return 0;
+
+    int mn = INT_MAX;
+
+    for(int k = i ; k <= j-1; k++)
+}
+
+int minScoreTriangulation(vector<int>& values) 
+{
+        
+}
+
+int main()
+{
+    return 0;
+}
