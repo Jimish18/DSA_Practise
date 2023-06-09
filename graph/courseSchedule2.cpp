253a7b1936af4bf71b1919badc97ca8f05e35793
jimish1806 <jimish957@gmail.com>
2023-06-09 14:08:41 +0530

    modified "course Schedule II" problem's Code

--- graph/courseSchedule2.cpp
+++ graph/courseSchedule2.cpp
@@ -7,37 +7,80 @@ Link - https://leetcode.com/problems/course-schedule-ii/
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isCycle(vector<vector<int>> &adj , vector<bool> &visited , vector<bool> &pathVisited , int node)
-{
-    visited[node] = true;
-    pathVisited[node] = true;
+//     bool isCycle(vector<vector<int>> &adj , vector<bool> &visited , vector<bool> &pathVisited , int node)
+// {
+//     visited[node] = true;
+//     pathVisited[node] = true;
 
-    for(auto x : adj[node])
-    {
-        if(!visited[x])
-        {
-            if(isCycle(adj,visited,pathVisited,x)) return true;
-        }
-        else if(pathVisited[x]) return true;
-    }
+//     for(auto x : adj[node])
+//     {
+//         if(!visited[x])
+//         {
+//             if(isCycle(adj,visited,pathVisited,x)) return true;
+//         }
+//         else if(pathVisited[x]) return true;
+//     }
 
-    pathVisited[node] = false;
-    return false;
-}
-
-void dfs(vector<vector<int>> &adj , vector<bool> &visited , int node , stack<int> &st)
-{
-    visited[node] = true;
-
-    for(auto x : adj[node])
-    {
-        if(!visited[x]) dfs(adj,visited,x,st);
-    }
-
-    st.push(node);
-}
+//     pathVisited[node] = false;
+//     return false;
+// }
 
 
+// // Topological Sort DFS Question
+// void dfs(vector<vector<int>> &adj , vector<bool> &visited , int node , stack<int> &st)
+// {
+//     visited[node] = true;
+
+//     for(auto x : adj[node])
+//     {
+//         if(!visited[x]) dfs(adj,visited,x,st);
+//     }
+
+//     st.push(node);
+// }
+
+
+// vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+// {
+//     vector<vector<int>> adj(numCourses);
+    
+//     for(auto x : prerequisites)
+//     {
+//         adj[x[0]].push_back(x[1]);
+//     }
+
+//     vector<bool> visited(numCourses,false);
+//     vector<bool> pathVisited(numCourses,false);
+
+//     for(int i = 0; i < numCourses; i++)
+//     {
+//         if(isCycle(adj,visited,pathVisited,i)) return {};
+//     }
+
+//     vector<bool> vis2(numCourses,false);
+//     stack<int> st;
+
+//     for(int i = 0; i < numCourses; i++)
+//     {
+//         if(!vis2[i])
+//         {
+//             dfs(adj,vis2,i,st);
+//         }
+//     }
+
+//     vector<int> ans;
+
+//     while(!st.empty())
+//     {
+//         ans.insert(ans.begin(),st.top());
+//         st.pop();
+//     }
+
+//     return ans;
+// }
+
+// Using TopoSort's Kahn's Algorithm 
+// Easiest Way
 vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
 {
     vector<vector<int>> adj(numCourses);
@@ -47,34 +90,42 @@ vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites)
         adj[x[0]].push_back(x[1]);
     }
 
-    vector<bool> visited(numCourses,false);
-    vector<bool> pathVisited(numCourses,false);
+    vector<int> inDegree(numCourses , 0);
 
-    for(int i = 0; i < numCourses; i++)
+    for(int i = 0 ; i < numCourses; i++)
     {
-        if(isCycle(adj,visited,pathVisited,i)) return {};
-    }
-
-    vector<bool> vis2(numCourses,false);
-    stack<int> st;
-
-    for(int i = 0; i < numCourses; i++)
-    {
-        if(!vis2[i])
+        for(auto x : adj[i])
         {
-            dfs(adj,vis2,i,st);
+            inDegree[x]++;
         }
     }
 
-    vector<int> ans;
+    queue<int> q;
 
-    while(!st.empty())
+    for(int i = 0; i < numCourses; i++)
     {
-        ans.push_back(st.top());
-        st.pop();
+        if(inDegree[i] == 0) q.push(i);
     }
 
-    return ans;
+    vector<int> topoOrder;
+
+    while(!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        topoOrder.insert(topoOrder.begin(),node);
+
+        for(auto x : adj[node])
+        {
+            inDegree[x]--;
+
+            if(inDegree[x] == 0) q.push(x);
+        }        
+    }
+
+    if(topoOrder.size() != numCourses) return {};
+
+    return topoOrder;
 }
 
 int main()
