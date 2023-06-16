f924093deba313f1044f5408b1a01d90d4eddec6
jimish1806 <jimish957@gmail.com>
2023-06-16 12:57:17 +0530

    solved "implementing Dijkstra's algorithm" problem on gfg using set method

--- graph/implementingDijkstra'sAlgorithm.cpp
+++ graph/implementingDijkstra'sAlgorithm.cpp
@@ -7,28 +7,65 @@ Link - https://practice.geeksforgeeks.org/problems/implementing-dijkstra-set-1-a
 #include <bits/stdc++.h>
 using namespace std;
 
+// priority Queue Method 
+// vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+// {
+//     vector<int> ans(V,1e9);
+//     priority_queue<pair<int,int> , vector<pair<int,int>>,greater<pair<int,int>>> minH;
+//     minH.push({0,S});
+//     ans[S] = 0; 
+
+//     while(!minH.empty())
+//     {
+//         int node = minH.top().second;
+//         int wt = minH.top().first;
+//         minH.pop();
+
+//         for(auto x : adj[node])
+//         {
+//             int edgeWt = x[1];
+//             int adjNode = x[0];
+
+//             if(wt + edgeWt < ans[adjNode])
+//             {
+//                 ans[adjNode] = wt + edgeWt;
+//                 minH.push({wt+edgeWt,adjNode});
+//             }
+//         }
+//     }
+
+//     return ans;
+// }
+
+
+// set method
 vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
 {
     vector<int> ans(V,1e9);
-    priority_queue<pair<int,int> , vector<pair<int,int>>,greater<pair<int,int>>> minH;
-    minH.push({0,S});
+    set<pair<int,int>> st;
+    st.insert({0,S});
     ans[S] = 0; 
 
-    while(!minH.empty())
+    while(!st.empty())
     {
-        int node = minH.top().second;
-        int wt = minH.top().first;
-        minH.pop();
+        auto it = *(st.begin());
+        int node = it.second;
+        int wt = it.first;
+        st.erase(it);
 
         for(auto x : adj[node])
         {
             int edgeWt = x[1];
             int adjNode = x[0];
 
-            if(wt + edgeWt < ans[adjNode])
+            if(edgeWt + wt < ans[adjNode])
             {
-                ans[adjNode] = wt + edgeWt;
-                minH.push({wt+edgeWt,adjNode});
+                // check if there already exist this node 
+                if(ans[adjNode] != 1e9) st.erase({ans[adjNode],adjNode});
+
+                ans[adjNode] = edgeWt + wt;
+                st.insert({ans[adjNode],adjNode});
+
             }
         }
     }
